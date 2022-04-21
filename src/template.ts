59633d4d9d4744f642c14e6d712ce2f1a864f0ce
Fabian Carlos <fabian.pow@gmail.com>
2022-04-21 15:27:39 -0400

    on touche end event

--- src/template.ts
+++ src/template.ts
@@ -211,7 +211,7 @@ export default `
           clearTimeout(timeoutSelection);
 
           if (globalSelection) {
-            timeoutSelection = setTimeout(() => globalSelection.removeAllRanges(), 900);
+            timeoutSelection = setTimeout(() => globalSelection.removeAllRanges(), 400);
           }
         });
 
